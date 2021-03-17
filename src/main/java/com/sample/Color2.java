1555158529f365e15841da82088c7136aea931cc
ko <ko.i@tis.co.jp>
2021-03-17 21:13:06 +0900

    update

--- src/main/java/com/sample/Color2.java
+++ src/main/java/com/sample/Color2.java
@@ -1,7 +1,21 @@
 package com.sample;
 
+/**
+ * 色を取得する<br>
+ *
+ *
+ * @author テスト君
+ */
 public class Color2 {
-    String getColor(int i) {
+   
+   /**
+    * 色取得ためのクラス。
+    *
+    *
+    * @param i 選択
+	* @return 結果
+    */
+	String getColor(int i) {
         if (i == 3) {
             return "赤";
         } else if (i == 4) {
